d59b331c16bb8ddb07efe8d63d6b010f99e96d13
jo32 <j.jolam@gmail.com>
2023-10-13 03:37:22 +0800

    feat: add links

--- app/page.tsx
+++ app/page.tsx
@@ -215,6 +215,23 @@ number: ${r?.metadata?.number}, generation: ${
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        <a
+          href="https://github.com/MegaPortal/closevector-nextjs-demo"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Source{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Get the source code of this demo on GitHub.
+          </p>
+        </a>
       </div>
     </main>
   )
